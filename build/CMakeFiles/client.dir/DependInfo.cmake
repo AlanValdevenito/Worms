e0d6418de297d77427414f5b369fada9c2014ebf
FedericoSolari <fsolariv@fi.uba.ar>
2023-10-22 15:32:09 -0300

    cambio de nombres de archivos y de implementacion del tp  threads

---
+++ build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/fede/Documentos/Taller/tp-worms/client/client.cpp" "CMakeFiles/client.dir/client/client.cpp.o" "gcc" "CMakeFiles/client.dir/client/client.cpp.o.d"
+  "/home/fede/Documentos/Taller/tp-worms/client/client_protocol.cpp" "CMakeFiles/client.dir/client/client_protocol.cpp.o" "gcc" "CMakeFiles/client.dir/client/client_protocol.cpp.o.d"
+  "/home/fede/Documentos/Taller/tp-worms/client/main.cpp" "CMakeFiles/client.dir/client/main.cpp.o" "gcc" "CMakeFiles/client.dir/client/main.cpp.o.d"
+  "/home/fede/Documentos/Taller/tp-worms/common/blockingqueue.cpp" "CMakeFiles/client.dir/common/blockingqueue.cpp.o" "gcc" "CMakeFiles/client.dir/common/blockingqueue.cpp.o.d"
+  "/home/fede/Documentos/Taller/tp-worms/common/broadcaster.cpp" "CMakeFiles/client.dir/common/broadcaster.cpp.o" "gcc" "CMakeFiles/client.dir/common/broadcaster.cpp.o.d"
+  "/home/fede/Documentos/Taller/tp-worms/common/dto.cpp" "CMakeFiles/client.dir/common/dto.cpp.o" "gcc" "CMakeFiles/client.dir/common/dto.cpp.o.d"
+  "/home/fede/Documentos/Taller/tp-worms/common/liberror.cpp" "CMakeFiles/client.dir/common/liberror.cpp.o" "gcc" "CMakeFiles/client.dir/common/liberror.cpp.o.d"
+  "/home/fede/Documentos/Taller/tp-worms/common/receiver_thread.cpp" "CMakeFiles/client.dir/common/receiver_thread.cpp.o" "gcc" "CMakeFiles/client.dir/common/receiver_thread.cpp.o.d"
+  "/home/fede/Documentos/Taller/tp-worms/common/resolver.cpp" "CMakeFiles/client.dir/common/resolver.cpp.o" "gcc" "CMakeFiles/client.dir/common/resolver.cpp.o.d"
+  "/home/fede/Documentos/Taller/tp-worms/common/resolvererror.cpp" "CMakeFiles/client.dir/common/resolvererror.cpp.o" "gcc" "CMakeFiles/client.dir/common/resolvererror.cpp.o.d"
+  "/home/fede/Documentos/Taller/tp-worms/common/sender_thread.cpp" "CMakeFiles/client.dir/common/sender_thread.cpp.o" "gcc" "CMakeFiles/client.dir/common/sender_thread.cpp.o.d"
+  "/home/fede/Documentos/Taller/tp-worms/common/socket.cpp" "CMakeFiles/client.dir/common/socket.cpp.o" "gcc" "CMakeFiles/client.dir/common/socket.cpp.o.d"
+  "/home/fede/Documentos/Taller/tp-worms/common/wrap_socket.cpp" "CMakeFiles/client.dir/common/wrap_socket.cpp.o" "gcc" "CMakeFiles/client.dir/common/wrap_socket.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/common/blockingqueue.cpp.o"
+  "CMakeFiles/server.dir/common/blockingqueue.cpp.o.d"
+  "CMakeFiles/server.dir/common/broadcaster.cpp.o"
+  "CMakeFiles/server.dir/common/broadcaster.cpp.o.d"
+  "CMakeFiles/server.dir/common/dto.cpp.o"
+  "CMakeFiles/server.dir/common/dto.cpp.o.d"
+  "CMakeFiles/server.dir/common/liberror.cpp.o"
+  "CMakeFiles/server.dir/common/liberror.cpp.o.d"
+  "CMakeFiles/server.dir/common/receiver_thread.cpp.o"
+  "CMakeFiles/server.dir/common/receiver_thread.cpp.o.d"
+  "CMakeFiles/server.dir/common/resolver.cpp.o"
+  "CMakeFiles/server.dir/common/resolver.cpp.o.d"
+  "CMakeFiles/server.dir/common/resolvererror.cpp.o"
+  "CMakeFiles/server.dir/common/resolvererror.cpp.o.d"
+  "CMakeFiles/server.dir/common/sender_thread.cpp.o"
+  "CMakeFiles/server.dir/common/sender_thread.cpp.o.d"
+  "CMakeFiles/server.dir/common/socket.cpp.o"
+  "CMakeFiles/server.dir/common/socket.cpp.o.d"
+  "CMakeFiles/server.dir/common/wrap_socket.cpp.o"
+  "CMakeFiles/server.dir/common/wrap_socket.cpp.o.d"
+  "CMakeFiles/server.dir/server/aceptador.cpp.o"
+  "CMakeFiles/server.dir/server/aceptador.cpp.o.d"
+  "CMakeFiles/server.dir/server/main.cpp.o"
+  "CMakeFiles/server.dir/server/main.cpp.o.d"
+  "CMakeFiles/server.dir/server/server.cpp.o"
+  "CMakeFiles/server.dir/server/server.cpp.o.d"
+  "CMakeFiles/server.dir/server/server_client.cpp.o"
+  "CMakeFiles/server.dir/server/server_client.cpp.o.d"
+  "CMakeFiles/server.dir/server/server_protocol.cpp.o"
+  "CMakeFiles/server.dir/server/server_protocol.cpp.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
