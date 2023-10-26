1a8597356f459749eeb79453cee9ebe49aef0589
ricomateo <mrico@fi.uba.ar>
2023-10-26 10:05:23 -0300

    agrego metodo broadcast a la clase Game

---
+++ server/game.cpp
@@ -0,0 +1,40 @@
+#include "game.h"
+
+
+Game::Game(BlockingQueue& queue, Broadcaster& broadcaster) :
+    game_finished(false),
+    common_queue(queue),
+    broadcaster(broadcaster),
+    world(World()) {
+        world.addBeam(3, 0, 6, 0.8);
+    }
+
+void Game::run() {
+
+    while (not game_finished) {
+        // dto = common_queue.try_pop();
+        // execute_command(dto);
+        update();
+        broadcast();
+    }
+}
+
+void Game::update() {
+    world.step();
+}
+
+void Game::broadcast() {
+    // por cada viga manda un Dto Viga a los senders
+    for (auto& beam: world.getBeams()) {
+        // cambiar los parametros a float
+        Viga *viga = new Viga((int)beam.getXCoordinate(),
+                              (int)beam.getYCoordinate(),
+                              (int)beam.getWidth(),
+                              (int)beam.getHeight());
+        broadcaster.addMessageToQueues(viga);
+    } 
+}
+
+void Game::stop() {
+    game_finished = true;
+}
---
+++ server/game.h
@@ -0,0 +1,32 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <stdio.h>
+#include <iostream>
+#include <exception>
+#include <list>
+#include "box2d/box2d.h"
+#include "world.h"
+#include "server_protocol.h"
+#include "thread.h"
+#include "blockingqueue.h"
+#include "broadcaster.h"
+#include "dto.h"
+#include "viga.h"
+
+class Game : public Thread {
+private:
+    BlockingQueue& common_queue;
+    Broadcaster& broadcaster;
+    World world;
+
+public:
+    bool game_finished;
+    Game(BlockingQueue& queue, Broadcaster& broadcaster);
+    void update();
+    void broadcast();
+    void run() override;
+    void stop() override;
+
+};
+#endif
