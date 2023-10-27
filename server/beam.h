6b3025150766b3992fa567a32b58a007efc37bb0
ricomateo <mrico@fi.uba.ar>
2023-10-27 13:09:33 -0300

    actualizo clase Beam

--- server/beam.h
+++ server/beam.h
@@ -2,35 +2,35 @@
 #define BEAM_H
 
 #include "box2d/box2d.h"
-#include <stdio.h>
-#include <iostream>
 #include <exception>
+#include <iostream>
+#include <stdio.h>
 
+#define SHORT_BEAM_WIDTH 3
+#define LONG_BEAM_WIDTH 3
+#define BEAM_HEIGHT 0.8
 
-
-enum BeamType {SHORT, LONG};
-
+enum BeamType { SHORT, LONG };
 
 class Beam {
 private:
-    b2Body *body;
-    float x;
-    float y;
-    float width;
-    float height;
-    float angle;
-    BeamType type;
+  b2Body *body;
+  float x;
+  float y;
+  float width;
+  float height;
+  float angle;
+  BeamType type;
 
 public:
-
-    // construye una viga centrada en el punto (x, y) rotada un ángulo <angle> (en grados) de tipo type.
-    Beam(b2Body *body, float x, float y, float angle, BeamType type);
-    float getXCoordinate();
-    float getYCoordinate();
-    float getWidth();
-    float getHeight();
-    float getAngle();
-    ~Beam();
+  // construye una viga centrada en el punto (x, y) rotada un ángulo <angle> (en
+  // grados) de tipo type.
+  Beam(b2Body *body, float x, float y, float angle, BeamType type);
+  float getXCoordinate();
+  float getYCoordinate();
+  float getWidth();
+  float getHeight();
+  float getAngle();
+  ~Beam();
 };
 #endif
-
