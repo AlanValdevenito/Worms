fdef0bef11ea7155b3cee498ece3b1cee0c174da
Alan Valdevenito <avaldevenito@fi.uba.ar>
2023-10-28 20:40:03 -0300

    Eliminado el directorio 'build' del seguimiento de Git

--- build/CMakeFiles/client.dir/common/Dtos/position.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/client.dir/common/Dtos/position.cpp.o: \
- /home/alan/Escritorio/Taller-de-Programacion-I/TPs/TP3\ -\ Worms/Worms/common/Dtos/position.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/server.dir/common/Dtos/position.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/server.dir/common/Dtos/position.cpp.o: \
- /home/alan/Escritorio/Taller-de-Programacion-I/TPs/TP3\ -\ Worms/Worms/common/Dtos/position.cpp \
- /usr/include/stdc-predef.h
