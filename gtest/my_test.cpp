f612f798646a15cfc1eee6c64c27e8089d682522
Alan Valdevenito <avaldevenito@fi.uba.ar>
2023-11-06 19:45:38 -0300

    Funciona la compilacion y ejecucion de los tests

--- gtest/my_test.cpp
+++ gtest/my_test.cpp
@@ -1,19 +1,19 @@
 #include <gtest/gtest.h>
 // #include "socket.h"
 #include "socket_mock.h"
-//#include "client.h"
-//#include "client_protocol.h"
-// #include "server_protocol.h"
+#include "client.h"
+#include "client_protocol.h"
+#include "server_protocol.h"
 
 TEST(MyTest, PassingTest)
 {
 
-    SocketMock skt;
-    // // Socket skt("127.0.0.1", "8080");
-    // ClientProtocol cp(std::ref(skt));
+    SocketMock skt_mock;
+    Socket skt("127.0.0.1", "8080");
+    ClientProtocol cp(std::ref(skt));
 
     // uint8_t a = 8;
-    // cp.sendPruebita(a);
+    // cp.sendPruebita(a); 
     // cp.recvPruebita();
 
     ASSERT_TRUE(true); // Esta aserción siempre será verdadera
